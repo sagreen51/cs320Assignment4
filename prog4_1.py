0083ffa96da5b484ed999aa35e68e53503a8a389
sagreen51 <s.a.green51@gmail.com>
2017-12-20 17:37:30 -0800

    test

--- prog4_1.py
+++ prog4_1.py
@@ -3,10 +3,10 @@ import os
 import random
 
 
-print("Assignment 4-1, Steven Green, s.a.green51@gmail.com")
+print("Assignment %#4-1, Steven Green, s.a.green51@gmail.com")
 fileName = sys.stdin.readline()
 fileName = fileName.strip()
-print(fileName)
+print("%s".format(fileName))
 test_file = open(fileName,"r+")
 test_in_file = test_file.readlines()
 print(test_in_file,end = "")
