93b8183448ef644f8ab523575cb7ebd1d9aa6fb5
sagreen51 <s.a.green51@gmail.com>
2017-12-20 17:26:55 -0800

    test

--- StackMachine.py
+++ StackMachine.py
@@ -1,1 +1,13 @@
-void temp
+class StackMachine:
+	def__init__(self):
+		self.list = []
+	def push(self, list):
+		self.list.insert(0,item)
+	def pop(self):
+		return self.list.pop(0)
+	def isEmpty(self):
+		return self.list == []
+	def peek(self):
+		return self.list[0]
+	def size(self):
+		return len(self.list)
