137daf5d458786c3c347ffa33eb22ff10697d4e6
sagreen51 <s.a.green51@gmail.com>
2017-12-20 18:00:37 -0800

    test

--- prog4_1.py
+++ prog4_1.py
@@ -3,10 +3,9 @@ import os
 import random
 
 x = '#'
-print("Assignment %c4-1, Steven Green, s.a.green51@gmail.com",x)
+print("Assignment "x"4-1, Steven Green, s.a.green51@gmail.com")
 fileName = sys.stdin.readline()
 '''fileName = fileName.strip()'''
-print(fileName.name)
 test_file = open(fileName,"r+")
 text_in_file = test_file.readlines()
 print(text_in_file)
