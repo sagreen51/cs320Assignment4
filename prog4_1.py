8e1223766ec7375b89d5ef9eecc9814d4e88fc4e
sagreen51 <s.a.green51@gmail.com>
2017-12-20 18:01:06 -0800

    test

--- prog4_1.py
+++ prog4_1.py
@@ -3,7 +3,7 @@ import os
 import random
 
 x = '#'
-print("Assignment "x"4-1, Steven Green, s.a.green51@gmail.com")
+print("Assignment ",x,"4-1, Steven Green, s.a.green51@gmail.com")
 fileName = sys.stdin.readline()
 '''fileName = fileName.strip()'''
 test_file = open(fileName,"r+")
