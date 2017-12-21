ffdfc3d3c956e85a4cd677283d25150113de595f
sagreen51 <s.a.green51@gmail.com>
2017-12-20 17:54:53 -0800

    test

--- prog4_1.py
+++ prog4_1.py
@@ -2,9 +2,10 @@ import sys
 import os
 import random
 
+
 print("Assignment 4-1, Steven Green, s.a.green51@gmail.com")
 fileName = sys.stdin.readline()
-fileName = fileName.strip()
+'''fileName = fileName.strip()'''
 print(fileName.name)
 test_file = open(fileName,"r+")
 text_in_file = test_file.readlines()
