59dd720a7031cc7f48e0f5d3f20701528bd81b64
sagreen51 <s.a.green51@gmail.com>
2017-12-20 23:12:48 -0800

    Final Submission

--- prog4_2.py
+++ prog4_2.py
@@ -8,7 +8,7 @@ print("4-1, Steven Green, s.a.green51@gmail.com")
 fileName = sys.argv[1]
 test_file = open(fileName,"r+")
 text_in_file = test_file.readlines()
-stack = StackMachine()
+#stack = StackMachine()
 #print("0",text_in_file)
 
 for r in text_in_file:
