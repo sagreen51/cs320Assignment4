bf6e9a7a8929a60e49d2891be2506d5af7d7ce4c
sagreen51 <s.a.green51@gmail.com>
2017-12-20 18:18:29 -0800

    test

--- prog4_1.py
+++ prog4_1.py
@@ -5,8 +5,8 @@ import random
 x = '#'
 print("Assignment ",x,end = '')
 print("4-1, Steven Green, s.a.green51@gmail.com")
-fileName = str(sys.argv)
-fileName = fileName.strip()
+fileName = sys.argv
+'''fileName = fileName.strip()'''
 print(fileName)
 test_file = open(fileName,"r+")
 text_in_file = test_file.readlines()
