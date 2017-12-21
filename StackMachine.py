89de1f67e49ae7530a201f0bfd1b4ee57e961ce4
sagreen51 <s.a.green51@gmail.com>
2017-12-20 22:32:03 -0800

    test

--- StackMachine.py
+++ StackMachine.py
@@ -1,15 +1,24 @@
 class StackMachine:
-	self.list = []
+	def __init__(self):
+        self.list = []
 
-	def push(self, list):
-		self.list.insert(0,item)
+	def push(self, x):
+		self.list.append(int(x))
 	def pop(self):
-		return self.list.pop(0)
+		return self.list.pop()
+    def add(self):
+        temp = StackMachine.pop(self) + StackMachine.pop(self)
+        StackMachine.push(self,temp)
 	def sub(self):
-        
+        temp = StackMachine.pop(self) - StackMachine.pop(self)
+        StackMachine.push(self,temp)
     def mul(self):
-
+        temp = StackMachine.pop(self) * StackMachine.pop(self)
+        StackMachine.push(self,temp)
     def div(self):
-
+        temp = StackMachine.pop(self) / StackMachine.pop(self)
+        StackMachine.push(self,temp)
     def mod(self): 
+        temp = StackMachine.pop(self) % StackMachine.pop(self)
+        StackMachine.push(self,temp)
 	
--- prog4_2.py
+++ prog4_2.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import StackMachine
 
 x = '#'
 print("Assignment",x,end = '')
