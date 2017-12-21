5d18291e24602d174a72bc423b6e043d41a71966
sagreen51 <s.a.green51@gmail.com>
2017-12-20 18:22:12 -0800

    test

--- prog4_1.py
+++ prog4_1.py
@@ -6,8 +6,8 @@ x = '#'
 print("Assignment",x,end = '')
 print("4-1, Steven Green, s.a.green51@gmail.com")
 fileName = sys.argv[1]
-'''fileName = fileName.strip()'''
-print(fileName)
+'''fileName = fileName.strip()
+print(fileName)'''
 test_file = open(fileName,"r+")
 text_in_file = test_file.readlines()
-print(text_in_file)
+print(" ".join(text_in_file))
