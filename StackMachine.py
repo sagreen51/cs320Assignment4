b19c22398c6f4db56346a30fc3f16ac93eb9bee1
sagreen51 <s.a.green51@gmail.com>
2017-12-20 22:22:09 -0800

    test

--- StackMachine.py
+++ StackMachine.py
@@ -1,13 +1,15 @@
 class StackMachine:
-	def__init__(self):
-		self.list = []
+	self.list = []
+
 	def push(self, list):
 		self.list.insert(0,item)
 	def pop(self):
 		return self.list.pop(0)
-	def isEmpty(self):
-		return self.list == []
-	def peek(self):
-		return self.list[0]
-	def size(self):
-		return len(self.list)
+	def sub(self):
+        
+    def mul(self):
+
+    def div(self):
+
+    def mod(self): 
+	
--- prog4_2.py
+++ prog4_2.py
@@ -1,1 +1,14 @@
-void temp
+import sys
+import os
+
+x = '#'
+print("Assignment",x,end = '')
+print("4-1, Steven Green, s.a.green51@gmail.com")
+fileName = sys.argv[1]
+test_file = open(fileName,"r+")
+text_in_file = test_file.readlines()
+
+#print("0",text_in_file)
+
+for r in text_in_file:
+    print(r.replace(" ", ","),end = "")
